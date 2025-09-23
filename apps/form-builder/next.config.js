d8e88779c332e160e84b9c0a4a08f42f04fffa21
Nam Vo <vhnam2504@gmail.com>
2025-09-23 18:00:35 +0700

    chore(form-builder): config bundle size

--- apps/form-builder/next.config.js
+++ apps/form-builder/next.config.js
@@ -5,6 +5,38 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+
+  experimental: {
+    optimizePackageImports: [
+      'lucide-react',
+      '@tanstack/react-query',
+      '@tanstack/react-table',
+    ],
+  },
+
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+            },
+          },
+        },
+      };
+    }
+
+    return config;
+  },
+
+  ...(process.env.NODE_ENV === 'production' && {
+    output: 'standalone',
+  }),
 };
 
 export default withNextIntl(nextConfig);
